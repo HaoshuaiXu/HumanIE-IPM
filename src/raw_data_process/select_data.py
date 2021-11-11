c8a7f9aef4ca6e2753c30657690a2eba83f0ec2a
StevenXu <hsh.xu@outlook.com>
2021-11-11 20:56:09 +0800

    Select friend sent id

--- src/raw_data_process/select_data.py
+++ src/raw_data_process/select_data.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
-sent_relation = pd.read_csv("/Users/xuhaoshuai/GitHub/HumanIE_IPM/data/rawdata/open_data(all)/sent_relation_train.txt", sep='\t', names=['sent_id', 'relation_id'])
-friend_sent_id = sent_relation[sent_relation['relation_id'] == '30']['sent_id'] # 30被识别为了字符串类型
-print(friend_sent_id)
+
+def select_by_relationid_from_opendata(datafilepath, to_select_relation_id:str, savedfilepath):
+    dataset = pd.read_csv(
+        filepath_or_buffer=datafilepath, 
+        sep='\t',
+        names=['sent_id', 'relation_id']
+        )
+    sent_id = dataset[dataset['relation_id'] == str(to_select_relation_id)]['sent_id'] # to_select_relation_id 在数据集中被识别为了字符串类型
+    sent_id.to_csv(savedfilepath, index=False)
+    
+    
+
+if __name__ == '__main__':
+    open_data_filepath = "/Users/xuhaoshuai/GitHub/HumanIE_IPM/data/raw_data/open_data(all)/sent_relation_train.txt"
+    select_by_relationid_from_opendata(
+        datafilepath=open_data_filepath,
+        to_select_relation_id='1',
+        savedfilepath="/Users/xuhaoshuai/GitHub/HumanIE_IPM/data/select_data/open_data_friend_sent_id.csv"
+    )
